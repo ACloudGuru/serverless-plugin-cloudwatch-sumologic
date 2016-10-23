9931e9e7b61f73ad4909dc08e7403f329c743897
Daniel Parker <dparker.tech@gmail.com>
2016-10-24 10:57:58 +1100

    Testing

--- src/index.js
+++ src/index.js
@@ -1,27 +1,56 @@
 'use strict';
 
+var path = require('path');
+var fs = require('fs');
+
 class Plugin {
     constructor(serverless, options) {
         this.serverless = serverless;
         this.options = options;
-
-        this.commands = {
-            deploy: {
-                lifecycleEvents: [
-                    'resources',
-                    'functions'
-                ]
-            }
-        };
 
         this.hooks = {
             'before:deploy:createDeploymentArtifacts': this.beforeDeployCreateDeploymentArtifacts.bind(this)
         };
     }
 
+    getEnvFilePath() {
+        return path.join(this.serverless.config.servicePath, 'sumologic-shipping-function');
+    }
+
     beforeDeployCreateDeploymentArtifacts() {
-        console.dir(this.serverless);
-        console.log('beforeDeployResources');
+        var functionPath = this.getEnvFilePath();
+
+        if (!fs.existsSync(functionPath)) {
+            fs.mkdirSync(functionPath);
+        }
+
+        var templatePath = path.resolve(__dirname, '../sumologic-function/handler.template.js');
+
+        var templateFile = fs.readFileSync(templatePath, 'utf-8');
+
+        var collectorUrl = this.serverless.service.custom.shipLogs.collectorUrl;
+
+        var handlerFunction = templateFile.replace('%collectorUrl%', collectorUrl);
+
+        fs.writeFileSync(path.join(functionPath, 'handler.js'), handlerFunction);
+
+        var functionNames = Object.keys(this.serverless.service.functions);
+
+        this.serverless.service.functions.sumologicShippingFunction = {
+            handler: '.temp/sumologic-shipping-function',
+            events: [],
+            name: 'sumologic-shipping-function'
+        };
+
+        console.log(functionNames);
+
+        console.log(collectorUrl);
+
+        console.log(functionPath);
+
+        console.log(templatePath);
+
+        console.log(handlerFunction);
     }
 }
 
---
+++ sumologic-function/handler.template.js
@@ -0,0 +1,81 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Remember to change the hostname and path to match your collection API and specific HTTP-source endpoint
+// See more at: https://help.sumologic.com/APIs/01Collector_Management_API
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+var sumoEndpoint = '%collectorUrl%';
+
+var https = require('https');
+var zlib = require('zlib');
+var url = require('url');
+
+exports.handler = function (event, context) {
+    var urlObject = url.parse(sumoEndpoint);
+
+    var options = {
+        'hostname': urlObject.hostname,
+        'path': urlObject.pathname,
+        'method': 'POST'
+    };
+
+    var zippedInput = new Buffer(event.awslogs.data, 'base64');
+
+    zlib.gunzip(zippedInput, function (e, buffer) {
+        if (e) { context.fail(e); }
+
+        var awslogsData = JSON.parse(buffer.toString('ascii'));
+
+        console.log(awslogsData);
+
+        if (awslogsData.messageType === "CONTROL_MESSAGE") {
+            console.log("Control message");
+            context.succeed("Success");
+        }
+
+        var requestsSent = 0;
+        var requestsFailed = 0;
+        var finalizeContext = function () {
+            var tot = requestsSent + requestsFailed;
+            if (tot == awslogsData.logEvents.length) {
+                if (requestsFailed > 0) {
+                    context.fail(requestsFailed + " / " + tot + " events failed");
+                } else {
+                    context.succeed(requestsSent + " requests sent");
+                }
+            }
+        };
+
+        var re = new RegExp(/(?:RequestId:|Z)\s+([\w\d\-]+)/);
+        var lastRequestID = null;
+        awslogsData.logEvents.forEach(function (val, idx, arr) {
+            var req = https.request(options, function (res) {
+                var body = '';
+                console.log('Status:', res.statusCode);
+                res.setEncoding('utf8');
+                res.on('data', function (chunk) { body += chunk; });
+                res.on('end', function () {
+                    console.log('Successfully processed HTTPS response');
+                    requestsSent++;
+                    finalizeContext();
+                });
+            });
+
+            req.on('error', function (e) {
+                console.log(e.message)
+                requestsFailed++;
+                finalizeContext();
+            });
+
+            var stream = awslogsData.logStream;
+            var group = awslogsData.logGroup;
+            var rs = re.exec(val.message);
+            if (rs !== null) {
+                lastRequestID = rs[1];
+            }
+            val.requestID = lastRequestID;
+            val.logStream = stream;
+            val.logGroup = group;
+            req.end(JSON.stringify(val));
+        });
+
+    });
+};
